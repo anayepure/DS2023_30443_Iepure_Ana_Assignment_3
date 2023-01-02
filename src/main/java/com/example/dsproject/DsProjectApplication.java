44a4b0650b34171d5f13a37474e22bb0b9b401ec
asus <ana_iepure@yahoo.com>
2023-01-02 22:36:22 +0200

    deleted assignemnt2 from main

--- src/main/java/com/example/dsproject/DsProjectApplication.java
+++ src/main/java/com/example/dsproject/DsProjectApplication.java
@@ -1,15 +1,6 @@
 package com.example.dsproject;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
